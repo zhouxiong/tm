9b8c4ab2a84445b638d7d85d2b669f234c378d88
'zhouxiong' <'706340089@qq.com'>
2020-01-08 10:38:25 +0800

    Excel工具类

--- src/main/java/io/renren/common/utils/ExcelReadUtils.java
+++ src/main/java/io/renren/common/utils/ExcelReadUtils.java
@@ -3,9 +3,13 @@ package io.renren.common.utils;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigInteger;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+import java.util.regex.Pattern;
 
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -32,12 +36,12 @@ public class ExcelReadUtils {
 	 * @param endRow   指定结束行
 	 * @return 返回读取列数据的set
 	 */
-	public static HashSet<String> getColumnSet(MultipartFile file, int column, int startRow,int endRow,int sheetAt) {
+	public static List<String> getColumnSet(MultipartFile file, int column, int startRow,int endRow,int sheetAt) {
 		Workbook wb = readExcel(file); // 文件
 		Sheet sheet = wb.getSheetAt(sheetAt); // sheet
 		int rownum = sheet.getPhysicalNumberOfRows(); // 行数
 		Row row = null;
-		HashSet<String> result = new HashSet<>();
+		List<String> result=new ArrayList<String>();
 		String cellData = null;
 		if (wb != null) {
 			for (int i = startRow - 1; i < rownum; i++) {
@@ -110,17 +114,22 @@ public class ExcelReadUtils {
 			} else {
 				//val = df.format(cell.getNumericCellValue()); // 数字型
 				// 返回数值类型的值
-				Object inputValue = null;// 单元格值
-				Long longVal = Math.round(cell.getNumericCellValue());
-				Double doubleVal = cell.getNumericCellValue();
-				if(Double.parseDouble(longVal + ".0") == doubleVal){   //判断是否含有小数位.0
-					inputValue = longVal;
-				}
-				else{
-					inputValue = doubleVal;
-				}
-				DecimalFormat dft = new DecimalFormat("#.####");    //格式化为四位小数，按自己需求选择；
-				val=dft.format(inputValue);
+//				Object inputValue = null;// 单元格值Math.round
+//				Long longVal = Math.round((cell.getNumericCellValue()));
+//				Double doubleVal = cell.getNumericCellValue();
+//				if(Double.parseDouble(longVal + ".0") == doubleVal){   //判断是否含有小数位.0
+//					inputValue = longVal;
+//				}
+//				else{
+//					inputValue = doubleVal;
+//				}
+//				DecimalFormat dft = new DecimalFormat("#.######");    //格式化为四位小数，按自己需求选择；
+//				val=dft.format(inputValue);
+				if(cell.getCellType() == Cell.CELL_TYPE_NUMERIC){
+	                return getRealStringValueOfDouble(cell.getNumericCellValue());
+	            }
+	            cell.setCellType(Cell.CELL_TYPE_STRING);
+	            return cell.getStringCellValue().trim();
 			}
 			break;
 		case HSSFCell.CELL_TYPE_STRING: // 文本类型
@@ -147,6 +156,29 @@ public class ExcelReadUtils {
 		}
 		return val;
 	}
+	
+    private static String getRealStringValueOfDouble(Double d) {
+        String doubleStr = d.toString();
+        boolean b = doubleStr.contains("E");
+        int indexOfPoint = doubleStr.indexOf('.');
+        if (b) {
+            int indexOfE = doubleStr.indexOf('E');
+            BigInteger xs = new BigInteger(doubleStr.substring(indexOfPoint
+                    + BigInteger.ONE.intValue(), indexOfE));
+            int pow = Integer.valueOf(doubleStr.substring(indexOfE
+                    + BigInteger.ONE.intValue()));
+            int xsLen = xs.toByteArray().length;
+            int scale = xsLen - pow > 0 ? xsLen - pow : 0;
+            doubleStr = String.format("%." + scale + "f", d);
+        } else {
+            java.util.regex.Pattern p = Pattern.compile(".0$");
+            java.util.regex.Matcher m = p.matcher(doubleStr);
+            if (m.find()) {
+                doubleStr = doubleStr.replace(".0", "");
+            }
+        }
+        return doubleStr;
+    }
 //	public static void main(String[] args) {
 //		HashSet<String> excelParamsSet = ExcelReadUtils.getColumnSet("G:\\renren-fast\\材料应力应变曲线-模板_wu_20191219-20191230112039.xlsx", 2, 1,20,0);//读取指定列的指定行数数据
 //	}
